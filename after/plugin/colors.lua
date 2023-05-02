b51d1466b9fa982bf51e0ad420ac37df5949fe3c
Bliss <mmoralesx86@gmail.com>
2023-05-02 12:37:34 -0500

    delete color function

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,13 +3,3 @@ require('rose-pine').setup({
     variant = "moon"
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-end
-
-ColorMyPencils()
